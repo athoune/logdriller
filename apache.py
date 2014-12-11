61ae594fd5371aebdcfd3b0513b081c8dc9f9353
Mathieu Lecarme <mlecarme@bearstech.com>
2014-12-11 22:17:43 +0100

    Handling raw request.

--- apache.py
+++ apache.py
@@ -2,9 +2,15 @@ import re
 import time
 
 
-APACHE = re.compile(r'^(?P<ip_address>\S+) - (?P<requesting_user>\S+) +\[(?P<timestamp>.*?)\] ("-"|("(?P<method>\w+) +(?P<request>\S+)( +HTTP/(?P<http_version>[0-9.]+))?")) (?P<response_code>\d{3}) (-|(?P<size>\d+)) "(?P<referrer>[^"]*)" "(?P<client>[^"]*)"')
+APACHE = re.compile(r'^(?P<ip_address>\S+) - (?P<requesting_user>\S+) '
+                    +r'\[(?P<timestamp>.*?)\]'
+                    +r' "(((?P<method>\w+) +(?P<request>\S+)'
+                    +r'( +HTTP/(?P<http_version>[0-9.]+))?)|(?P<rawrequest>.*?))"'
+                    +r' (?P<response_code>\d{3}) (-|(?P<size>\d+)) '
+                    +r'"(?P<referrer>[^"]*)" "(?P<client>[^"]*)"')
 
-MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
 
 def parse_apache_date(txt):
@@ -44,7 +50,9 @@ def _parse(regex, line):
 if __name__ == '__main__':
     lines = [
         '183.15.4.245 - - [08/Dec/2014:17:54:01 +0100] "-" 408 - "-" "-"',
-        '82.66.246.250 - tracsearch [09/Dec/2014:16:06:34 +0100] "GET /?q=debian&start=&end=&facet_status=&facet__type=&facet_component=&facet_domain=&facet_priority=&facet_user=vschmitt&facet_keywords=&facet_path= HTTP/1.1" 200 7715 "https://search.bearstech.com/?q=debian&start=&end=" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20100101 Firefox/35.0"'
+        '82.66.246.250 - tracsearch [09/Dec/2014:16:06:34 +0100] "GET /?q=debian&start=&end=&facet_status=&facet__type=&facet_component=&facet_domain=&facet_priority=&facet_user=vschmitt&facet_keywords=&facet_path= HTTP/1.1" 200 7715 "https://search.bearstech.com/?q=debian&start=&end=" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20100101 Firefox/35.0"',
+        '71.6.165.200 - - [10/Dec/2014:04:03:21 +0100] "quit" 501 301 "-" "-"',
+        '69.70.201.74 - - [10/Dec/2014:01:28:15 +0100] "  " 200 726 "-" "-"',
     ]
     p = parse_apache(lines[0])
     print p
@@ -52,5 +60,8 @@ if __name__ == '__main__':
     p = parse_apache(lines[1])
     print p
     assert p['requesting_user'] == 'tracsearch'
-    print parse_apache_date('09/Dec/2014:16:06:34 +0100')
 
+    p = parse_apache(lines[2])
+    print p
+    p = parse_apache(lines[3])
+    print p
